2311a130fe7c16adc62feb854317704be4a8cc66
Anthony Rurup <tcrurup@gmail.com>
2019-05-23 03:33:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if self.all_items_in_cart == 0
+      if self.class.all_items_in_cart == 0
         resp.write "Your cart is empty"
       else
         resp.write self.class.all_items_in_cart
